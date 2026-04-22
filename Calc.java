af8c11e8300894c1d3127d8f012c51753cec3e32
Sushmita Chakraborty <sushmita.chakraborty@keross.com>
2026-04-22 18:42:01 +0530

    Inheritance Classes added

---
+++ Calc.java
@@ -0,0 +1,14 @@
+public class Calc {
+    public int add(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public int sub(int num1, int num2){
+        if(num1 > num2){
+            return num1 - num2;
+        }else{
+            return num2 - num1;
+        }
+        
+    }
+}
---
+++ Demo3.java
@@ -0,0 +1,8 @@
+
+//For Inheritance
+public class Demo3 {
+    public static void main(String a[]){
+        
+    }
+    
+}
